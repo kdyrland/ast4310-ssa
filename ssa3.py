553b2d43a25a2f3d7de5ef9b82e59c68f0137ece
kdyrland <31306308+kdyrland@users.noreply.github.com>
2017-09-28 15:28:41 +0200

    Update ssa3.py

--- ssa3.py
+++ ssa3.py
@@ -1,5 +1,5 @@
 from numpy import zeros, arange, array, str, exp, log, logspace
-from matplotlib.pyplot import plot, show, xlabel, ylabel, title, hold, legend, yscale, xscale, ylim, xlim
+from matplotlib.pyplot import plot, show, xlabel, ylabel, title, hold, legend, yscale, xscale, ylim, xlim, grid
 from scipy import special
 
 
@@ -8,6 +8,19 @@ h = 6.62607e-27             # Planck's constant [erg s]
 c = 2.99792e10              # Speed of light [cm/s]
 wav = arange(1000,20801,200)
 b = zeros(wav.shape)
+
+B = 2.
+tau = arange(0.01,10.01, 0.01)
+intensity = zeros(tau.shape)
+
+for I0 in range(4,-1,-1):
+    intensity[:] = I0 * exp(-tau[:]) + B*(1-exp(-tau[:]))
+    plot(tau, intensity, linewidth=1.0, label = 'intensity I(0) = ' + str(I0))
+xlabel(r'Optical depth $\tau$', size=14)
+ylabel('Intensity', size=14)
+legend(fontsize=12)
+grid()
+show()
 
 
 def planck(temp, wav):
@@ -30,21 +43,23 @@ for i in range(4):
 
 for i in range(4):
     vau[i, :] = voigt(a[i], u[:])
-    plot(u[:], vau[i, :], label = 'a = ' + str(a[i]))
+    plot(u[:], vau[i, :], linewidth=1.0, label = 'a = ' + str(a[i]))
 ylim(0,1)
 xlim(-10,10)
-legend(fontsize=8)
+legend(fontsize=12)
 xlabel('u', size=14)
-ylabel('Voigt function', size=12)
+ylabel('Voigt function', size=14)
+grid()
 show()
 
 for i in range(4):
     vau[i, :] = voigt(a[i], u[:])
-    plot(u[:], vau[i, :], label = 'a = ' + str(a[i]))
+    plot(u[:], vau[i, :], linewidth=1.0, label = 'a = ' + str(a[i]))
 yscale('log')
-legend(fontsize=8, loc = 8)
+legend(fontsize=12, loc = 8)
 xlabel('u', size=14)
-ylabel('Logarithmic voigt function', size=12)
+ylabel('Logarithmic voigt function', size=14)
+grid()
 show()
 
 Ts = 5700.      # solar surface temperature
@@ -59,9 +74,10 @@ intensity = zeros(u.shape)
 for i in range(201):
     tau = tau0 * voigt(a, u[i])
     intensity[i] = planck(Ts,wav) * exp(-tau) + planck(Tl, wav)*(1.- exp(-tau))
-    plot(u, intensity)
+    plot(u, intensity, linewidth=1.0)
 xlabel('u', size=14)
-ylabel('Intensity', size=12)
+ylabel('Intensity', size=14)
+grid()
 show()
 
 logtau0 = arange(-2,2.1,0.5)
@@ -70,10 +86,11 @@ for itau in range(9):
     for i in range(201):
         tau = 10.**(logtau0[itau]) * voigt(a, u[i])
         intensity[i] = planck(Ts,wav) * exp(-tau) + planck(Tl,wav)*(1.-exp(-tau))
-    plot(u,intensity, label = r'$\log{(\tau_0)} = $' + str(logtau0[itau]))
-legend(loc=3, fontsize=8)
+    plot(u,intensity, linewidth=1.0, label = r'$\log{(\tau_0)} = $' + str(logtau0[itau]))
+legend(loc=3, fontsize=10)
 xlabel('u', size=14)
-ylabel('Intensity', size=12)
+ylabel('Intensity', size=14)
+grid()
 show()
 
 for iwav in range(1,4):
@@ -83,10 +100,11 @@ for iwav in range(1,4):
             tau = 10.**(logtau0[itau]) * voigt(a,u[i])
             intensity[i] = planck(Ts,wav) * exp(-tau) + planck(Tl,wav)*(1.-exp(-tau))
         intensity = intensity / intensity[0]
-        plot(u,intensity[:], linewidth=1., label = r'$\log{(\tau_0)} = $' + str(logtau0[itau]))
-legend(loc=3, fontsize=5)
+        plot(u, intensity[:], linewidth=1.0, label = r'$\log{(\tau_0)} = $' + str(logtau0[itau]))
+legend(loc=3, fontsize=6)
 xlabel('u', size=14)
-ylabel('I/I(0)', size=12)
+ylabel('I/I(0)', size=14)
+grid()
 show()
 
 
@@ -109,16 +127,18 @@ u = arange(-200,200.4,0.4)
 a = 0.1
 tau0 = 1.0e2
 intensity = profile(a, tau0, u)
-plot(u, intensity)           # absorption line
-xlabel('u')
-ylabel('Profile')
+plot(u, intensity, linewidth=1.0)           # absorption line
+xlabel('u', size=14)
+ylabel('Profile', size=14)
+grid()
 show()
 
 # relative
 reldepth = (intensity[0]-intensity)/intensity[0]
-plot(u, reldepth)           # emission line
-xlabel('u')
-ylabel('Relative depth')
+plot(u, reldepth, linewidth=1.0)           # emission line
+xlabel('u', size=14)
+ylabel('Relative depth', size=14)
+grid()
 show()
 
 eqw = sum(reldepth)*0.4
@@ -134,9 +154,10 @@ for i in range(61):
     reldepth = (intensity[0] - intensity) / intensity[0]
     eqw[i] = sum(reldepth)*0.4
 
-plot(tau0,eqw)
-xlabel(r'$\tau_0$', size=12)
-ylabel(r'Equivalent width $W_{\lambda}$', size=12)
+plot(tau0,eqw, linewidth=1.0)
+xlabel(r'$\tau_0$', size=14)
+ylabel(r'Equivalent width $W_{\lambda}$', size=14)
+grid()
 xscale('log')
 yscale('log')
 show()
